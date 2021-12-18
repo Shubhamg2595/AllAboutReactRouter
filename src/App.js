5534e48938ae6abaf5287d38bb480fea77df86f2
Shubham Gupta <shubhamg2595@gmail.com>
2021-12-18 18:50:35 +0530

    basic routing setup complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import {Link} from 'react-router-dom'
+
 function App() {
   return (
     <div>
-      <h1>My React Router APP!</h1>
+      <h1>My React Router APP about BookKeepers!</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">
+          Invoices
+        </Link>
+        <Link to="/expenses">
+          Expenses
+        </Link>
+      </nav>
     </div>
   );
 }
---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import App from "./App";
+import Expenses from "./components/Expenses";
+import Invoices from "./components/Invoices";
+export default function AppRoutes() {
+  return (
+    <>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="invoices" element={<Invoices />} />
+          <Route path="expenses" element={<Expenses />} />
+        </Routes>
+    </>
+  );
+}
