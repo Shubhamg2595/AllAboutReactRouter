cc551014a04f3eab8a5605d86b6220d2801f0eec
Shubham Gupta <shubhamg2595@gmail.com>
2021-12-18 20:47:17 +0530

    index ROutes

--- src/Routes.js
+++ src/Routes.js
@@ -12,6 +12,14 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
         <Route path="expenses" element={<Expenses />} />
@@ -20,3 +28,13 @@ export default function AppRoutes() {
     </>
   );
 }
+
+// index routes in routerv6
+// Notice it has the index prop instead of a path. That's because the index route shares the path of the parent. That's the whole point--it doesn't have a path.
+
+// Maybe you're still scratching your head. There are a few ways we try to answer the question "what is an index route?". Hopefully one of these sticks for you:
+
+// Index routes render in the parent routes outlet at the parent route's path.
+// Index routes match when a parent route matches but none of the other children match.
+// Index routes are the default child route for a parent route.
+// Index routes render when the user hasn't clicked one of the items in a navigation list yet.
